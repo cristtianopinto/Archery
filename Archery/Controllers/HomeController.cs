56ef08ffd83f6a88c2d7bc678734a6e0a02e54b1
Cris <cristtianodesouza@gmail.com>
2018-09-24 15:36:05 +0200

    MVC - avec Index e about 1 - creation de model Info 2 - intanciacao de Info no metodo About do HomeControler 3 - Acesso aos valores de Info

---
+++ Archery/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Archery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            var modelInfo = new Info
+            {
+                DevName = "PINTO",
+                ContactMail = "cris@email.com",
+                CreatedDate = DateTime.Today
+            };
+            return View(modelInfo);
+        }
+        
+    }
+}
