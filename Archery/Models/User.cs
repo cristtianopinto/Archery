f4f5791dc70cd720231b90fd1fb7009d14e3e450
M. Pinto <cristtianodesouza@gmail.com>
2018-09-26 12:28:00 +0200

    1 - marcation de datatype et display danqs la class User pour utiliser en HTML Helpers au Subscribe 2 - Utilisation de HTML Helpers par la creation de chaps

--- Archery/Models/User.cs
+++ Archery/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,18 +8,28 @@ namespace Archery.Models
 {
     public abstract class User
     {
+
         public int ID { get; set; }
 
+        [Display(Name = "Adresse mail")]
         public string Mail { get; set; }
 
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)] // para gerar o html helper do tipo password
         public string Password { get; set; }
 
+        [Display(Name = "Confirmation du mot de passe")]
+        [DataType(DataType.Password)]
         public string ConfirmedPassword { get; set; }
 
+        [Display(Name = "Nom")]
         public string LastName { get; set; }
 
+        [Display(Name = "Prénom")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
     }
 }
