2b139f53c3d2b0a553c60e7098bbe9a65b8d1c42
Cristtiano <cristtianopinto@gmail.com>
2018-10-01 17:46:32 +0200

    Action Subscribe pour WeaponController

---
+++ Archery/Areas/BackOffice/Controllers/WeaponController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Areas.BackOffice.Controllers
+{
+    public class WeaponController : Controller
+    {
+        // GET: BackOffice/Weapon
+        public ActionResult Subscribe()
+        {
+            return View();
+        }
+    }
+}
