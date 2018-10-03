4a138f4d6cb81f6653086c1df76ea6f8e3ef3bbc
Cristtiano <cristtianopinto@gmail.com>
2018-10-03 14:47:59 +0200

    creation champ email et Password

--- Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
+++ Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Archery.Validators;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,9 @@ namespace Archery.Areas.BackOffice.Models
 {
     public class AuthenticationLoginViewModels
     {
+        [Required(ErrorMessage = "Le champ {0} ets obligatoire.")]       
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Le champ {0} ets obligatoire.")]
+        public string Password { get; set; }
     }
 }
